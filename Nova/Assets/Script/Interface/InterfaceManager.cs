9e3df180b439cd824d43eb5df84f05de04c2bab2
MD <werwerwe@web.de>
2016-08-02 17:52:48 +0200

    starting

--- Nova/Assets/Script/Interface/InterfaceManager.cs
+++ Nova/Assets/Script/Interface/InterfaceManager.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class InterfaceManager : MonoBehaviour {
 
     public WeaponInterface weaponInterface;
-
+    public Backpack backPack;
+    public Hotbar hotbar;
 
 	// Use this for initialization
 	void Start () {
--- Nova/Assets/Script/Interface/WeaponInterface.cs
+++ Nova/Assets/Script/Interface/WeaponInterface.cs
@@ -1,7 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using Assets.Script.ItemSystem;
 
 public class WeaponInterface : MonoBehaviour {
+
+    public Text projectilesText;
+    public Text magazinesText;
+    public Text ammoText;
+
+    ItemToolValues toolValues;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +18,20 @@ public class WeaponInterface : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if(toolValues != null){
+            projectilesText.text = toolValues.loadedProjectiles.ToString();
+	        magazinesText.text = "0";
+            ammoText.text = toolValues.Ammo.name;
+        }
+        else
+        {
+            projectilesText.text = "0";
+            magazinesText.text = "0";
+            ammoText.text = "";
+        }
 	}
+
+    public void setToolValues(ItemToolValues toolValues){
+        this.toolValues = toolValues;
+    }
 }
