b5ea4be0c17349a898c4eb4fca2881a3c478d002
MD <werwerwe@web.de>
2016-08-28 21:51:45 +0200

    Speichern laden angefangen

---
+++ Nova/Assets/Script/SaveFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[Serializable]
+class SaveFile
+{
+    public List<GameObject> ships = new List<GameObject>();
+}
---
+++ Nova/Assets/Script/SaveLoadGUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveLoadGUI : MonoBehaviour {
+
+	public void ClickSave()
+    {
+        SaveLoad.Save();
+    }
+}
